53ae57362aaf52f7e338704b9a52731271b00b9e
Koki <u2daijibrother@gmail.com>
2015-05-02 04:48:35 +0900

    Initial Commit.

---
+++ WaitingList/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WaitingList.MvcApplication" Language="C#" %>
---
+++ WaitingList/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WaitingList.Startup))]
+namespace WaitingList
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

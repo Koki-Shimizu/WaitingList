8d2fcf54afb4b99c6d3d5f2f977259addcd76223
Koki <u2daijibrother@gmail.com>
2015-05-03 04:59:38 +0900

    change display name on site list.

--- WaitingList/Models/Site.cs
+++ WaitingList/Models/Site.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,20 @@ namespace WaitingList.Models
     public class Site
     {
         public int Id { get; set; }
+
+        [DisplayName("サイト名")]
         public string Name { get; set; }
+
+        [DisplayName("説明")]
         public string Description { get; set; }
+
+        [DisplayName("作成日")]
         public DateTime CreatedDate { get; set; }
+
+        [DisplayName("未完了件数")]
         public int UncompletedCount { get; set; }
+
+        [DisplayName("完了日時")]
         public Nullable<DateTime> CompletedDate { get; set; }
     }
 }

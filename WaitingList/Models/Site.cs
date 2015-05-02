7f09042a0a5a811e81b5a1b73c6bd38a4aeb8868
Koki <u2daijibrother@gmail.com>
2015-05-03 05:35:21 +0900

    modify column attributes.

--- WaitingList/Models/Site.cs
+++ WaitingList/Models/Site.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -11,18 +12,22 @@ namespace WaitingList.Models
         public int Id { get; set; }
 
         [DisplayName("サイト名")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "名前は50文字以内で記載してください。")]
         public string Name { get; set; }
 
         [DisplayName("説明")]
+        [StringLength(255, ErrorMessage = "説明文は255文字以内で記載してください。")]
         public string Description { get; set; }
 
         [DisplayName("作成日")]
+        [DataType(DataType.Date)]
         public DateTime CreatedDate { get; set; }
 
         [DisplayName("未完了件数")]
         public int UncompletedCount { get; set; }
 
         [DisplayName("完了日時")]
-        public Nullable<DateTime> CompletedDate { get; set; }
+        [DisplayFormat(NullDisplayText = "進行中")]
+        public DateTime? CompletedDate { get; set; }
     }
 }
